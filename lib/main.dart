0c3af49b1c3aa774c81064add7edebc86f3ac8c3
nitin-singla <nitingithub@gmail.com>
2023-06-15 23:57:33 +0530

    Created an initial homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 116, 37, 172),
+        body: Column(
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Let's learn flutter!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Start quiz'),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
